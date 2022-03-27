0b52aed264a35549534acb8ce2bbc822e8dcf859
sureshshaw <sureshcsegcect@gmail.com>
2022-03-27 22:44:51 +0530

    Update BaseTest.java

--- src/test/java/com/midtrans/demo/tests/BaseTest.java
+++ src/test/java/com/midtrans/demo/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package com.midtrans.demo.tests;
 
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
